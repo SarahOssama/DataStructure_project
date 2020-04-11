e0e29b136f353c72cd9b69764a5c9a9174aab4b8
khadijaswelam <khadijaswelam2000@gmail.com>
2020-04-11 12:26:47 +0200

    rest

--- Restaurant/Rest/Restaurant.cpp
+++ Restaurant/Rest/Restaurant.cpp
@@ -409,7 +409,9 @@ void Restaurant::Interactive_mode()
 																	 //problem and it's better for complexity to make another list
 	{
 		itoa(CurrentTime, timestep, 10);
-		pGUI->PrintMessage(timestep);
+		string TSmsg = "Current TS =";
+		TSmsg += timestep;
+		pGUI->PrintMessage(TSmsg);
 
 
 		ExecuteEvents(CurrentTime);
@@ -465,23 +467,27 @@ void Restaurant::Interactive_mode()
 		//////the bag of orders will  also contain finished orders.. think of doing another fn to loop if there still smth in service??
 		//////or maybe doing another bag for finished orders and then avoid looping ?? 
 		//  when calling the filldrawing fn().. it will loop and draw also this array (ne5aliha te3mel keda ma3 eel tanyin !! ;) 
-
 		
-		while (In_Service_Orders_B.FromInService_to_Finished()->getItem() !=nullptr)   // loop on In service bag to transfer orders from in serv to finished 
+		while (In_Service_Orders_B.FromInService_to_Finished())// gives me a ptr to head .. 
+			                                                   // if the head ==nullptr ..then the bag is empty..will not enter the while loop
 		{
-			///// modified by khadija .. initially without ".getItem()" in while condition and line 466
-			///// because it gives compilation error .. it can not convert Node<order*> to order*
-			Order* ord= In_Service_Orders_B.FromInService_to_Finished()->getItem();  
-			if (CurrentTime == ord->GetServTime() + 5)
+			if (In_Service_Orders_B.FromInService_to_Finished()->getItem() != nullptr)   // loop on In service bag to transfer orders from in serv to finished 
 			{
-				Order* temp = ord;
-				Finished_Orders_B.AddNode(ord);
-				ord->setStatus(DONE);
-				delete temp;
-			}
-			
-		}
+				///// modified by khadija .. initially without ".getItem()" in while condition and line 466
+				///// because it gives compilation error .. it can not convert Node<order*> to order*
+				Order* ord = In_Service_Orders_B.FromInService_to_Finished()->getItem();
+				if (CurrentTime == ord->GetServTime() + 5)
+				{
+					Order* temp = ord;
+					Finished_Orders_B.AddNode(ord);
+					ord->setStatus(DONE);
+					delete temp;
+				}
 
+			}
+			In_Service_Orders_B.FromInService_to_Finished()->getNext();
+
+		}
 		
 		FillDrawingList();
 		pGUI->UpdateInterface();
